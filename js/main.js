5f14285a973ec7f898e74487c69006393ba3e797
davidtrikic <41009202+davidtrikic@users.noreply.github.com>
2021-04-10 01:51:44 +0200

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,199 +1,155 @@
 
-let overlayDiv = document.getElementById('image-overlay');
+// Components
+const imagesContainer = document.getElementById('images-container');
+const overlayDiv = document.getElementById('image-overlay');
+const overlayImg = document.getElementById('overlay-img');
+const img_base = document.getElementById('image_1');
 
+let border = 8;
+let pos = 0;
 
-let imagesContainer = document.getElementById('images-container');
+// Set the width of overlay image to match the initial container size
+overlayImg.style.width = imagesContainer.offsetWidth - 8 + "px";
 
-let image2 = document.getElementById('image_2');
-let width = imagesContainer.offsetWidth;
-
-// dodaj na onload
-image2.style.width = imagesContainer.offsetWidth - 8 + "px";
-
-
-
-// window.addEventListener('resize', changeOverlayWidth);
-
-
+// Main function call on page load
 window.addEventListener('onload', compareImages(overlayDiv));
 
+function compareImages(imgDiv) {
 
-    function compareImages(img) {
+  // Set the width of overlay image to match the container width on window resize
+  window.addEventListener('resize', function() {
+    overlayImg.style.width = imagesContainer.offsetWidth  - border + "px";
 
-      let pos = 0;
-      window.addEventListener('resize', changeOverlayWidth);
+    if (pos > imagesContainer.offsetWidth) {
+      imgSlider.style.left = imagesContainer.offsetWidth - (imgSlider.offsetWidth / 2) + "px";
+      overlayDiv.style.width = imagesContainer.offsetWidth - border + "px"
+  }});
 
-      function changeOverlayWidth() {
-        image2.style.width = imagesContainer.offsetWidth  - 8 + "px";
-        if (pos > imagesContainer.offsetWidth) {
-         
-          slider.style.left = (imagesContainer.offsetWidth) - (slider.offsetWidth / 2) + "px";
-          console.log('slider: ' + slider.style.left);
-          overlayDiv.style.width = imagesContainer.offsetWidth  - 8 + "px"
-
-        };
-
-      }
-
-     
-      var slider, img, clicked = 0, w, h;
-      /*get the width and height of the img element*/
-      w = img.offsetWidth;
-      h = img.offsetHeight;
-      /*set the width of the img element to 50%:*/
-      img.style.width = (w / 2) + "px";
-      /*create slider:*/
-      slider = document.createElement("DIV");
-      slider.setAttribute("class", "image-slider");
-      /*insert slider*/
-      img.parentElement.insertBefore(slider, img);
-      /*position the slider in the middle:*/
-      slider.style.top = (h / 2) - (slider.offsetHeight / 2) + "px";
-      slider.style.left = (w / 2) - (slider.offsetWidth / 2) + "px";
-      /*execute a function when the mouse button is pressed:*/
-      slider.addEventListener("mousedown", slideReady);
-      /*and another function when the mouse button is released:*/
-      window.addEventListener("mouseup", slideFinish);
-      /*or touched (for touch screens:*/
-      slider.addEventListener("touchstart", slideReady);
-      /*and released (for touch screens:*/
-      window.addEventListener("touchend", slideFinish);
-      
-      function slideReady(e) {
-        /*prevent any other actions that may occur when moving over the image:*/
-        e.preventDefault();
-        /*the slider is now clicked and ready to move:*/
-        clicked = 1;
-        /*execute a function when the slider is moved:*/
-        window.addEventListener("mousemove", slideMove);
-        window.addEventListener("touchmove", slideMove);
-    
-      }
-      function slideFinish() {
-        /*the slider is no longer clicked:*/
-        clicked = 0;
-
-     
-      }
-      function slideMove(e) {
-        // var pos;
-        /*if the slider is no longer clicked, exit this function:*/
-        if (clicked == 0) return false;
-        /*get the cursor's x position:*/
-        if(e.type == "mousemove") {
-          // If the type of event is mousemove get cursor position, if not get touch position
-          pos = getCursorPos(e);
-          console.log("Pos_2 " + pos);
-        } else {
-          pos = getTouchPos(e);
-        }
-        /*prevent the slider from being positioned outside the image:*/
-        if (pos < 0) pos = 0;
-        if (pos > imagesContainer.offsetWidth) pos = imagesContainer.offsetWidth;
-
-        // if (pos > w) pos = w;
-        /*execute a function that will resize the overlay image according to the cursor:*/
-        // console.log("pos: " + pos);
-        slide(pos);
-
-      }
-
-      function getTouchPos(e) {
-        var a, x, y = 0;
-        e = e || window.event;
-        /*get the x positions of the image:*/
-        a = img.getBoundingClientRect();
-        /*calculate the cursor's x coordinate, relative to the image:*/
-        y = parseInt(e.touches[0].clientX);
-        /*consider any page scrolling:*/
-        x = x - window.pageXOffset;
-        return y;
-      }
-
-      function getCursorPos(e) {
-        var a, x, y = 0;
-        e = e || window.event;
-        /*get the x positions of the image:*/
-        a = img.getBoundingClientRect();
-        /*calculate the cursor's x coordinate, relative to the image:*/
-       
-        x = e.pageX - a.left;
-
-        
-        // x = e.pageX;
-        // y = e.pageX;
-        // console.log("Y: " + y);
-        /*consider any page scrolling:*/
-        x = x - window.pageXOffset;
+  var imgSlider, imgDiv, isClicked = 0, imgDiv_width, imgDiv_height;
+  // Store overlay div width and height
+  imgDiv_width = imgDiv.offsetWidth;
+  imgDiv_height = imgDiv.offsetHeight;
+  // Set the overlay div to 50% width
+  imgDiv.style.width = (imgDiv_width / 2) + "px";
+  // Create slider
+  imgSlider = document.createElement("div");
+  imgSlider.setAttribute("class", "image-slider");
+  // Insert slider
+  imgDiv.parentElement.insertBefore(imgSlider, imgDiv);
+  // Position the slider
+  imgSlider.style.top = (imgDiv_height / 2) - (imgSlider.offsetHeight / 2) + "px";
+  imgSlider.style.left = (imgDiv_width / 2) - (imgSlider.offsetWidth / 2) + "px";
+  // Activate slider on mouse click
+  imgSlider.addEventListener("mousedown", sliderStart);
+  // Deactivacte slider when mouse button is not clicked
+  window.addEventListener("mouseup", sliderStop);
+  // Activate slider on touch screens
+  imgSlider.addEventListener("touchstart", sliderStart);
+  // Deactivate slider on touch screens
+  window.addEventListener("touchend", sliderStop);
   
-        return x;
-      }
-      function slide(x) {
-        /*resize the image:*/
-        img.style.width = x + "px";
-        /*position the slider:*/
-        slider.style.left = img.offsetWidth - (slider.offsetWidth / 2) + "px";
+  function sliderStart(e) {
+    // Prevent any actions on a apge while slider is active
+    e.preventDefault();
+    // Set slider state to clicked
+    isClicked = 1;
+    // Execute a function when the slider is moved
+    window.addEventListener("mousemove", moveSlider);
+    window.addEventListener("touchmove", moveSlider);
+  }
 
-      }
+  // Stop the slider if the button is no longer clicked
+  function sliderStop() {
+    isClicked = 0;
+  }
 
-      window.addEventListener('resize', function() {
-        
-        // if (pos > imagesContainer.offsetWidth) {
-         
-        //   slider.style.left = imagesContainer.offsetWidth + "px";
-        //   console.log('slider: ' + slider.style.left);
-        // };
-      })
+  function moveSlider(e) {
+ 
+    // Exit the function if the slider is no longer clicked
+    if (isClicked == 0) return false;
+    // Get the cursor's X position
+    // If the type of event is mousemove get cursor position, if not get touch position
+    if(e.type == "mousemove") {
+      pos = getCursorPos(e);
+    } else {
+      pos = getTouchPos(e);
+    };
+    // Prevent the slider control from eing positioned outside of the image container
+    if (pos < 0) pos = 0;
+    if (pos > imagesContainer.offsetWidth) pos = imagesContainer.offsetWidth;
 
-    }
-  // }
-// W3Schools Code ends here
+    // Execute a function that will resize the overlay image according to the cursor
+    slide(pos);
+  }
 
-// Image upload functions
+  function getTouchPos(e) {
+    var a, x, y = 0;
+    e = e || window.event;
+    // Get the coordinates of the overlay div element
+    a = imgDiv.getBoundingClientRect();
+    // Calculate the cursor's X coordinate, relative to the image position
+    y = parseInt(e.touches[0].clientX) - a.left;
+    // Substract any page scrolling, if any
+    x = x - window.pageXOffset;
+    return y;
+  }
 
-let button_1 = document.getElementById('upload-button-1');
-let button_2 = document.getElementById('upload-button-2');
+  function getCursorPos(e) {
+    var a, x, y = 0;
+    e = e || window.event;
+    // Get the coordinates of the overlay div element
+    a = imgDiv.getBoundingClientRect();
+    // Calculate the cursor's X coordinate, relative to the image position
+    x = e.pageX - a.left;
+    // Substract any page scrolling, if any
+    x = x - window.pageXOffset;
+    return x;
+  }
 
-// Function call on window load
+  function slide(x) {
+    // Resize the overlay div
+    imgDiv.style.width = x + "px";
+    // Set the slider control position one the edge of the overlay image
+    imgSlider.style.left = imgDiv.offsetWidth - (imgSlider.offsetWidth / 2) + "px";
+  }
+};
+
+// Image upload function
+
+const button_1 = document.getElementById('upload-button-1');
+const button_2 = document.getElementById('upload-button-2');
+
+// Execute the function on page load
 window.addEventListener('load', imageUpload());
 
-// Upload button (page refresh)
-document.getElementById('reset').addEventListener('click', function(){ location.reload();});
+
 
 // Image Upload function
 function imageUpload() {
-    
-    let img_1 = document.getElementById('image_2');
-    let img_2 = document.getElementById('image_1');
-    //  call function on event change
-
-    // Add event listeners on buttons, Get image URLs on img src tag
-
+    // Add event listener on buttons; Get image's URL on the img src tag
     button_1.addEventListener('change', function(){
         if (this.files && this.files[0]) {
-            img_1.onload = () => {
-                URL.revokeObjectURL(img_1.src);
+            overlayImg.onload = () => {
+                URL.revokeObjectURL(overlayImg.src);
             }
-            img_1.src = URL.createObjectURL(this.files[0]);
+            overlayImg.src = URL.createObjectURL(this.files[0]);
         }
     });
 
     button_2.addEventListener('change', function(){
         if (this.files && this.files[0]) {
-            img_2.onload = () => {
-                URL.revokeObjectURL(img_2.src);
+            img_base.onload = () => {
+                URL.revokeObjectURL(img_base.src);
             }
-            img_2.src = URL.createObjectURL(this.files[0]);
+            img_base.src = URL.createObjectURL(this.files[0]);
         }
-    })
-
-
+    });
 }
 
 // File upload button label customization
 
-let file_1Label = document.getElementById('file-1-label');
-let file_2Label = document.getElementById('file-2-label');
+const file_1Label = document.getElementById('file-1-label');
+const file_2Label = document.getElementById('file-2-label');
 
 button_1.addEventListener('change', function(){
     file_1Label.textContent = this.files[0].name;
@@ -205,9 +161,10 @@ button_2.addEventListener('change', function(){
 
 // Add border button 
 
-let borderButton = document.getElementById('border-button');
+const borderButton = document.getElementById('border-button');
 
 borderButton.addEventListener('click', function() {
+  console.log('asdf');
   overlayDiv.classList.toggle('slider-border');
   changeLabel();
 });
@@ -220,3 +177,5 @@ function changeLabel() {
     else {borderButton.innerHTML = "Add border"};
 }
 
+// Reset button
+document.getElementById('reset').addEventListener('click', function(){ location.reload();});
