91097b221f4e12895b1db5f31d2e2ab896c5ac82
davidtrikic <41009202+davidtrikic@users.noreply.github.com>
2021-04-11 19:56:05 +0200

    removed extras

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ function compareImages(imgDiv) {
   // Create slider
   imgSlider = document.createElement("div");
 ////////
-  imgSlider.setAttribute("class", "image-slider");
+  imgSlider.setAttribute("class", "image-slider slider-position");
   // Insert slider
   imgDiv.parentElement.insertBefore(imgSlider, imgDiv);
   // Position the slider
@@ -196,12 +196,6 @@ function changeLabel() {
 // Reset button
 document.getElementById('reset').addEventListener('click', function(){ 
   overlayDiv.style.width = (imagesContainer.offsetWidth / 2) + "px";
-  let slider = document.getElementsByClassName('position')[0];
+  let slider = document.getElementsByClassName('slider-position')[0];
   slider.style.left = (imagesContainer.offsetWidth / 2) - (slider.offsetWidth / 2) + "px";
 });
-
-// Slider switch button 
-
-// function sliderSwitch(slider) {
-
-// }
